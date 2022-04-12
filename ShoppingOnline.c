576ca8d297e5f88aa666d0db499bbb8524581184
saltyDario <94072049+saltyDario@users.noreply.github.com>
2022-04-12 18:00:49 +0200

    V0.2
    non ho ancora idea di cosa sto facendo, spero di averla in questi giorni.

--- ShoppingOnline.c
+++ ShoppingOnline.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <windows.h>
+//#include <windows.h>
 #include <stdlib.h>
 #include <string.h>
 
-void red(HANDLE hConsole){
+/*void red(HANDLE hConsole){
     SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
 }
 
@@ -17,25 +17,53 @@ void blue(HANDLE hConsole){
 
 void reset(HANDLE hConsole, WORD saved_attributes){
     SetConsoleTextAttribute(hConsole, saved_attributes);
+}*/
+
+int ricaricaSaldo(int ricarica, char *utente){
+
+    FILE *fp;
+    char fileUtenti[20] = "utenti.txt";
+    fp = fopen(fileUtenti, "a+");
+
+    int check = 0;
+
+    int saldoAttuale = 0;
+    char tmpUT[99];
+    char typeUT[99];
+
+    while(!feof(fp)){
+        fscanf(fp, "%s %d %s\n", tmpUT, &saldoAttuale, typeUT);
+        //printf("\n%s %d %s\n", tmpUT, saldoAttuale, typeUT);
+        if(strcmp(tmpUT, utente) == 0){
+            saldoAttuale = saldoAttuale + ricarica;
+            fprintf("%s %d %s", tmpUT, saldoAttuale, typeUT);
+            check = 1;
+            printf("%d %s", saldoAttuale, tmpUT);
+            break;
+        }
+    }
+
+    return check;
 }
 
 int LogIn(char *utente, char *password){
-    //printf("%s %s", utente, password);
     int UTcheck = 0;
-    char tmpUT[] = "init";
+    char tmpUT[99];
 
     int PWcheck = 0;
-    char tmpPW[] = "init";
+    char tmpPW[99];
     
     int check = 0;
 
+    int counter = 1;
+    
     FILE *fp;
     char fileUtenti[20] = "utenti.txt";
     char filePassword[20] = "passwords.txt";
 
     fp = fopen(fileUtenti, "r");
     while (!feof(fp)){
-        fscanf(fp, "%s\n", &tmpUT);
+        fscanf(fp, "%s\n", tmpUT);
 
         if(strcmp(tmpUT, utente) == 0){
             UTcheck = 1;
@@ -46,7 +74,7 @@ int LogIn(char *utente, char *password){
 
     fp = fopen(filePassword, "r");
     while (!feof(fp)){
-        fscanf(fp, "%s\n", &tmpPW);
+        fscanf(fp, "%s\n", tmpPW);
         
         if(strcmp(tmpPW, password) == 0){
             PWcheck = 1;
@@ -59,7 +87,7 @@ int LogIn(char *utente, char *password){
         check = 1;
     }
 
-    printf("\n%d %d\n", UTcheck, PWcheck);
+    //printf("\n%d %d\n", UTcheck, PWcheck);
     return check;
 }
 
@@ -76,7 +104,7 @@ int Register(char *utente, char *password){
 
     fpU = fopen(fileUtenti, "r");
     while (!feof(fpU)){
-        fscanf(fpU, "%s\n", &tmpUT);
+        fscanf(fpU, "%s\n", tmpUT);
 
         if(strcmp(tmpUT, utente) == 0){
             UTduplicate = 1;
@@ -86,11 +114,11 @@ int Register(char *utente, char *password){
     fclose(fpU);
 
     if(UTduplicate == 0){
-        fpU = fopen(fileUtenti, "w");
-        fpP = fopen(filePassword, "w");
+        fpU = fopen(fileUtenti, "a");
+        fpP = fopen(filePassword, "a");
 
-        fprintf(fpU, "\n%s", utente);
-        fprintf(fpP, "\n%s", password);
+        fprintf(fpU, "%s 0 Utente\n", utente);
+        fprintf(fpP, "%s\n", password);
 
         check = 1;
 
@@ -103,94 +131,148 @@ int Register(char *utente, char *password){
 
 int main(int argc, char *argv[]){
 
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    /*HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
-    WORD saved_attributes;
+    WORD saved_attributes;*/
 
-    /* Attributi console */
+    /* Attributi console 
     GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
-    saved_attributes = consoleInfo.wAttributes;
+    saved_attributes = consoleInfo.wAttributes; */
 
     char choose_str[99];
+
     char utente[99];
     char pass[99];
+
     int checkL = 0;
     int checkR = 0;
+    int check = 0;
 
-    red(hConsole);
+    int menuChoice;
+    int choice;
+
+    int tmp;
+
+    //red(hConsole);
     printf("\t\tProgetto LASD Aprile 2022\n\t\t     Shopping Online\n\t\t  Dario Morace N86003778\n");
-    reset(hConsole, saved_attributes);
+    //reset(hConsole, saved_attributes);
 
-    blue(hConsole);
+    //blue(hConsole);
     printf("Benvenuto, inserisci Login per autenticarti o Registrazione per registrarti.\n");
-    reset(hConsole, saved_attributes);
+    //reset(hConsole, saved_attributes);
     scanf("%s", choose_str);
-    //printf("%s\n", choose_str);
 
     while(strcmp(choose_str, "Login") != 0 && strcmp(choose_str, "Registrazione") != 0){
-    red(hConsole);
+    //red(hConsole);
     printf("Scelta non corretta, riprova.\n");
-    reset(hConsole, saved_attributes);
+    //reset(hConsole, saved_attributes);
     scanf("%s", choose_str);
     }
 
     if(strcmp(choose_str, "Login") == 0){
         
         while(checkL == 0){
-            blue(hConsole);
+            //blue(hConsole);
             printf("Inserisci il tuo nome utente:\n");
-            reset(hConsole, saved_attributes);
+            //reset(hConsole, saved_attributes);
             scanf("%s", utente);
-            //printf("\nSubito dopo: %s\n", utente);
 
-            blue(hConsole);
+            //blue(hConsole);
             printf("Inserisci la tua password:\n");
-            reset(hConsole, saved_attributes);
+            //reset(hConsole, saved_attributes);
             scanf("%s", pass);
-            //printf("\nSubito dopo: %s\n", pass);
 
             checkL = LogIn(utente, pass);
 
             if(checkL == 1){
-                green(hConsole);
+                system("clear");
+                //green(hConsole);
                 printf("Login effettuato con successo!.\n");
-                reset(hConsole, saved_attributes);
+                //reset(hConsole, saved_attributes);
             }else{
-                red(hConsole);
+                //red(hConsole);
                 printf("Le credenziali sono incorrette o non presenti. Se non lo hai fatto registrati.\n");
-                reset(hConsole, saved_attributes);
+                //reset(hConsole, saved_attributes);
             }
         }
     }
     else if(strcmp(choose_str, "Registrazione") == 0){
         while(checkR == 0){
-            blue(hConsole);
+            //blue(hConsole);
             printf("Inserisci un nome utente:\n");
-            reset(hConsole, saved_attributes);
+            //reset(hConsole, saved_attributes);
             scanf("%s", utente);
-            //printf("\nSubito dopo: %s\n", utente);
 
-            blue(hConsole);
+            //blue(hConsole);
             printf("Inserisci una password:\n");
-            reset(hConsole, saved_attributes);
+            //reset(hConsole, saved_attributes);
             scanf("%s", pass);
-            //printf("\nSubito dopo: %s\n", pass);
 
             checkR = Register(utente, pass);
 
             if(checkR == 1){
-                green(hConsole);
+                //green(hConsole);
                 printf("Registrazione effettuata con successo!.\n");
-                reset(hConsole, saved_attributes);
+                //reset(hConsole, saved_attributes);
+
+                checkL = LogIn(utente, pass);
+
+                if(checkL == 1){
+                    //green(hConsole);
+                    printf("Login effettuato con successo!.\n");
+                    //reset(hConsole, saved_attributes);
+                }
             }else{
-                red(hConsole);
+                //red(hConsole);
                 printf("Il nome inserito è già stato preso!. Riprova.\n");
-                reset(hConsole, saved_attributes);
+                //reset(hConsole, saved_attributes);
             }
         }
     }
 
+    printf("Benvenuto nello Shop, %s.\n", utente);
+    while(menuChoice != 4){
 
-    getch();
+        printf("Menu:\n1]Visiona lo shop.\n2]Visiona carrello.\n3]Gestisci saldo.\n4]Exit\n");
+        scanf("%d", &menuChoice);
+        switch (menuChoice)
+        {
+        case 1:
+            /* code */
+
+        case 2:
+            /* code */
+
+        case 3:
+            system("clear");
+            printf("Gestione saldo di %s. Digita una scelta.\n", utente);
+            while(choice != 4){
+                printf("Menu:\n1]Ricarica saldo.\n2]Visiona saldo.\n3]Scarica saldo.\n4]Exit\n");
+                scanf("%d", &choice);
+                switch (choice)
+                {
+                case 1:
+                    printf("Inserisci la quantita' da ricaricare.\n");
+                    scanf("%d", &tmp);
+                    check = ricaricaSaldo(tmp, utente);
+
+                    if(check == 1){
+                        printf("La ricarica di %d è stata effettuata.", tmp);
+                        continue;
+                    }else{
+                        //boh
+                        continue;
+                    }
+                default:
+                    printf("Questa scelta non esiste, riprova.\n");
+                }
+            }
+        case 4:
+            break;
+        default:
+            printf("Questa scelta non esiste, riprova.\n");
+        }
+    }
+    //getch();
     return 0;
 }
